c98d64389b5560b0c56132780bb1a130399feb83
Franky Yeung <frankyeung1996@gmail.com>
2020-09-29 11:29:17 +0800

    Update the P2BLanding Page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { NavigationBar } from './components/NavigationBar';
 import { Home } from './Home';
 import { About } from './About';
 import { NoMatch } from './NoMatch';
+import P2BLandingPage  from './pages/P2BLandingPage';
+import P2BInputInfoPage from './pages/P2BInputInfoPage';
+import P2BConfirmPage from './pages/P2BConfirmPage';
+import P2BUploadDocPage from './pages/P2BUploadDocPage';
 import FullHeight from "react-full-height";
 
 function App() {
@@ -17,6 +21,10 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
+          <Route path="/p2blanding" component={P2BLandingPage} />
+          <Route path="/p2binput" component={P2BInputInfoPage} />
+          <Route path="/p2bconfirm" component={P2BConfirmPage} />
+          <Route path="/p2bupload" component={P2BUploadDocPage} />
           <Route component={NoMatch} />
         </Switch>
       </Router>
---
+++ src/pages/P2BLandingPage.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
+export default class P2BInputInfoPage extends React.Component {
+    render() {
+      return (
+        <Container fluid>
+          <Row>
+            <Col><h2>P2B Crowd-lending</h2></Col>
+          </Row>
+          <Row>
+            <Col><p>Registration</p></Col>
+            <Col><p>Submission</p></Col>
+            <Col><p>Review & Approval</p></Col>
+            <Col><p>Publish</p></Col>
+            <Col><p>Get Funded</p></Col>
+          </Row>
+          <Row>
+            <Col><ProgressBar now={20} /></Col>
+          </Row>
+
+          <br />
+
+          <Row>
+            <Col><h3>Create a new request in less than 10 mins!</h3></Col>
+          </Row>
+          <Row>
+            <Col>
+              <p>To Ensure a smooth application process, you may prepare the following documents in advance:</p>
+              <ol>
+                <li>Proof of Identity</li>
+                <li>Proof of Residence</li>
+                <li>Bank statement of the previous 6 months</li>
+                <li>Audited Financial Statements</li>
+                <li>Other Mandatory Documents (i.e. Sole Prop. Declaration Or Certified Copy of Partnership Deed)</li>
+              </ol>
+            </Col>
+            <Col><Button variant="danger">Begin</Button></Col>
+          </Row>
+
+          <hr /> 
+          <Row>
+            <Col><h3>OR</h3></Col>
+          </Row>
+
+          <Row>
+            <Col><Button variant="danger">Use Existing Profile</Button></Col>
+          </Row>
+
+          <Row>
+            <Col><small>The P2B Crowd-lending service is operated under Securities and Future Ordiance(‘SFO’) 103(2) by Securities and Futures Commission which allows small-scale retail crowdfunding not exceedding HKD 5 Milllions over a 12 months period in Hong Kong.</small></Col>
+          </Row>
+        </Container>
+      );
+    }
+}
